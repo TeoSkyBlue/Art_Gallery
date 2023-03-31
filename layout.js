0b38fc9109af81a22bfb341d041820eacc7e7e9c
TeoSkyBlue <70573650+TeoSkyBlue@users.noreply.github.com>
2023-03-31 20:01:16 +0300

    removed a bunch of useless console.logs lol

--- layout.js
+++ layout.js
@@ -5,7 +5,6 @@ const header_pages = document.querySelectorAll(".nav-link.link-dark.shadow-sm, #
 
 for (header_page of header_pages){
     if (header_page.nodeName == "BUTTON"){
-        console.log("in button");
         header_page.addEventListener("mouseup", change_frame);
     }
 }
@@ -13,7 +12,7 @@ for (header_page of header_pages){
 function change_frame(e){
     item = e.currentTarget;
     let source = item.id;
-    console.log(source);
+    
     if(source =="#"){
         return
     }
@@ -21,8 +20,8 @@ function change_frame(e){
         source = "visit/visit.html"
     }
     let iframe_item = document.querySelector("iframe");
-    console.log(iframe_item);
+    
     iframe_item.src = source;
-    console.log(iframe_item.source);
+    
 }
 
