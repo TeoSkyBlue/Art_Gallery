5206e5ebb0b90a7323fc8ac041092824c84e28b4
TeoSkyBlue <70573650+TeoSkyBlue@users.noreply.github.com>
2023-05-12 20:53:57 +0300

    Now properly using Express-handlebars .hbs engine

--- app.mjs
+++ app.mjs
@@ -1,6 +1,5 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
-import hbs from 'hbs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import home_router from './routes/home_r.mjs';
@@ -14,6 +13,7 @@ const __dirname = path.dirname(__filename);
 
 
 const app = express();
+
 const port = process.env.PORT || 8080;
 
 app.engine('hbs', engine({extname:'hbs'}));
@@ -23,8 +23,6 @@ app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public/stylesheets'), { type: 'text/css', }));
 app.use(express.static(path.join(__dirname, 'public/scripts'), { type: 'text/javascript', }));
-
-hbs.registerPartials(__dirname + '/views/partials');
 
 
 app.set('views', path.join(__dirname, 'views'));
