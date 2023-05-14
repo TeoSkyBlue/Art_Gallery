dd1e7db4483c72ebaf17021b95c8c5dcf615e413
TeoSkyBlue <70573650+TeoSkyBlue@users.noreply.github.com>
2023-05-14 15:41:06 +0300

    First definition of mongoDb ArtGallery Schema

---
+++ models/art_gallery_schema.mjs
@@ -0,0 +1,123 @@
+import mongoose from 'mongoose';
+
+
+const adminSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required:true
+    }
+});
+
+const postSchema = new mongoose.Schema({
+    edited: [{ // idea is edited[0] is publication date and rest is dates of edits.
+        type: Date,
+        required: true
+    }],
+    images: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Images'
+    }],   // schema pic has 1 cardinality, needs update.
+    content: { 
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    post_type: Boolean, //looks and feels off. Will re-evaluate later.
+    authors: [{  //refers to editors too, author is editor[0].
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Admin',
+        required: true
+    }]
+});
+
+
+const imagesSchema = new mongoose.Schema({
+    name: String,
+    image: {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+
+const roomSchema = new mongoose.Schema({
+    number: {
+        type: Number,
+        unique: true,
+        required: true
+    },
+
+    genre: String,
+
+    capacity: {
+        max: {
+            type: Number,
+            required: true
+        },
+        current: {
+            type: Number
+        }
+    },
+
+    description: String,
+
+    images: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Images'
+    }],
+});
+
+const artSchema = new mongoose.Schema({
+    summary: String,
+    genre: String,
+    creation_date: Date,
+    name: String,
+    image: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Images'
+    },
+});
+
+const artistSchema = new mongoose.Schema({
+    info: String,
+    profile_pic: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Images'
+    },
+    born: Date,
+    died: Date,
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    }
+});
+
+
+const galleryModel = 
+{
+    admin: mongoose.model("Admin", adminSchema),
+    artist: mongoose.model("Artist", artistSchema),
+    art: mongoose.model("Art", artSchema),
+    post: mongoose.model("Post", postSchema),
+    image: mongoose.model("Image", imagesSchema),
+    room: mongoose.model("Room", roomSchema)
+
+};
+
+export default galleryModel;
+
+
