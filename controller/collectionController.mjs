2797c5e0a47d0661926bf09ce8e81e9e80efc177
TeoSkyBlue <70573650+TeoSkyBlue@users.noreply.github.com>
2023-05-17 20:14:26 +0300

    implemented join logic on art-image collections.

--- controller/collectionController.mjs
+++ controller/collectionController.mjs
@@ -2,9 +2,10 @@
 import db from '../models/mongo_conn.mjs';
 
 
-export async function displayArtworks(req, res){
+export async function displayImages(req, res){
     try{
         const docs =  await db.collection('images').find().limit(9).toArray();
+        console.log(docs);
         const artworks = docs.map(doc => ({img: doc.image.data.toString('base64'), type: doc.name}));
         res.render('collection', { artworks });
     }catch(err){
@@ -12,4 +13,35 @@ export async function displayArtworks(req, res){
         res.send(err);
     }
 
-}
+};
+
+
+export async function displayArtworks(req, res){
+    try{
+        const docs =  await db.collection('arts')
+        .aggregate([
+            {$lookup:
+                {
+                    from: 'images',
+                    localField: 'image',
+                    foreignField: '_id',
+                    as: "art_image"
+                }
+            }
+        ])
+        .limit(9)
+        .toArray();
+        console.log(docs[0].art_image[0].length);
+        // const artworks = [];
+        // for (let document of docs){
+        //     artworks.push(document.art_image[0].map(doc => ({img: doc.art_image.image.data.toString('base64'), type: doc.name})));
+        // }
+        
+        const artworks = docs.map(doc => ({img: doc.art_image[0].image.data.toString('base64'), type: doc.name}));
+
+        res.render('collection', { artworks });
+    }catch(err){
+        console.log(err);
+        res.send(err);
+    }
+};
