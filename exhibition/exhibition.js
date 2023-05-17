02a441c3514b4ae11ac63d10aeceb75590062926
up1066456 <up1066456@upnet.gr>
2023-05-17 19:01:32 +0300

    exhibition start
    slider and cards for rooms. also small changes in the other folders

---
+++ exhibition/exhibition.js
@@ -0,0 +1,55 @@
+// slider 
+
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+let active_i = 1;
+
+// Javascript for image slider manual navigation
+var manualNav = function(manual){
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+  });
+
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+    active_i = i;
+  });
+});
+
+// Javascript for image slider autoplay navigation
+var repeat = function(activeClass){
+  let active = document.getElementsByClassName('active');
+
+  var repeater = () => {
+    setTimeout(function(){
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove('active');
+      });
+
+    slides[active_i].classList.add('active');
+    btns[active_i].classList.add('active');
+    active_i++;
+
+    if(slides.length == active_i){
+      active_i = 0;
+    }
+    if(active_i >= slides.length){
+      return;
+    }
+    repeater();
+  }, 5000);
+  }
+  repeater();
+}
+repeat();
