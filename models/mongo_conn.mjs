57860ea871fa62aa61ada03f57b61032be6ca354
TeoSkyBlue <70573650+TeoSkyBlue@users.noreply.github.com>
2023-05-28 15:10:45 +0300

    secure cookies fix.

--- models/mongo_conn.mjs
+++ models/mongo_conn.mjs
@@ -20,7 +20,9 @@ export const my_session = session({
     store: new MongoStore({ 
         mongoUrl: process.env.MONGO_URL_PROD,
         ttl: 60 * 26, //26 mins cookie within db
-        
+        crypto: {
+            secret: process.env.MONGO_STORE_SECRET
+        }
     }), 
     cookie: {
         secure: true
